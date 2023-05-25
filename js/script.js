3a75d07801c93f06906a182b81aaa33592bd195f
william <124389603+william-simard@users.noreply.github.com>
2023-05-25 13:39:49 +0000

    done so far

--- js/script.js
+++ js/script.js
@@ -1,5 +1,26 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2023 william simard All rights reserved
 //
-// Created by: Mr. Coxall
+// Created by: wiilliam simard
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+
+/**
+ * This function displays the slider value.
+ */
+
+// program to generate fibonacci series up to a certain number
+
+// take input from the user
+const fibonacci = (n) => {
+  let a = 0,
+    b = 1,
+    c = n
+
+  for (let i = 2; i <= n; i++) {
+    c = a + b
+    a = b
+    b = c
+  }
+
+  return c
+}
